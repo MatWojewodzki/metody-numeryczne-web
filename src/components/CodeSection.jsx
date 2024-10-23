c45905ae1d0a109411be22e4f3b70fbb38e54a94
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2024-10-23 18:48:54 +0200

    Further improved font sizing in the code section

--- src/components/CodeSection.jsx
+++ src/components/CodeSection.jsx
@@ -6,32 +6,40 @@ import { CPP_OUTPUT, PYTHON_OUTPUT } from '../source_code/consoleOutput';
 const CodeSection = () => {
   return (
     <Section title={'Rozwiązania zadania za pomocą programowania'}>
-      <ol className='list-decimal list-inside'>
+      <ol className='list-decimal list-inside text-xl'>
         <li className='mb-12'>
-          <h3 className='text-xl inline'>Schemat działania programu zapisany w pseudokodzie:</h3>
-          <CodeBlock filename={'pseudokod'} language={'text'}>
-            {PSEUDOCODE}
-          </CodeBlock>
+          <h3 className='inline'>Schemat działania programu zapisany w pseudokodzie:</h3>
+          <div className='text-base'>
+            <CodeBlock filename={'pseudokod'} language={'text'}>
+              {PSEUDOCODE}
+            </CodeBlock>
+          </div>
         </li>
 
         <li className='mb-12'>
-          <h3 className='text-xl inline'>Rozwiązanie zadania w języku C++</h3>
-          <CodeBlock filename={'.cpp'} language={'cpp'}>
-            {CPP_CODE}
-          </CodeBlock>
-          <CodeBlock filename={'Console Output'} language={'text'}>
-            {CPP_OUTPUT}
-          </CodeBlock>
+          <h3 className='inline'>Rozwiązanie zadania w języku C++</h3>
+          <div className="text-base">
+            <CodeBlock filename={'.cpp'} language={'cpp'}>
+              {CPP_CODE}
+            </CodeBlock>
+            <CodeBlock filename={'Console Output'} language={'text'}>
+              {CPP_OUTPUT}
+            </CodeBlock>
+          </div>
+
         </li>
 
         <li className='mb-12'>
-          <h3 className='text-xl inline'>Rozwiązanie zadanie w języku Python z wykorzystaniem bibliotek NumPy oraz SciPy (dodatkowo wykonanie wykresu)</h3>
-          <CodeBlock filename={'.py'} language={'python'}>
-            {PYTHON_CODE}
-          </CodeBlock>
-          <CodeBlock filename={'Console Output'} language={'text'}>
-            {PYTHON_OUTPUT}
-          </CodeBlock>
+          <h3 className='inline'>Rozwiązanie zadanie w języku Python z wykorzystaniem bibliotek NumPy oraz SciPy (dodatkowo wykonanie wykresu)</h3>
+          <div className="text-base">
+            <CodeBlock filename={'.py'} language={'python'}>
+              {PYTHON_CODE}
+            </CodeBlock>
+            <CodeBlock filename={'Console Output'} language={'text'}>
+              {PYTHON_OUTPUT}
+            </CodeBlock>
+          </div>
+
           <img
             className='mx-auto rounded-md'
             alt='Wykres przedstawiający pole między wykresami funkcji'
