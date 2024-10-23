692d9a0142427144403bd24818ff834bc18e0889
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2024-10-23 18:46:37 +0200

    Improved font sizing

--- src/components/SpreadsheetSection.jsx
+++ src/components/SpreadsheetSection.jsx
@@ -3,13 +3,13 @@ import Section from "./Section"
 const SpreadsheetSection = () => {
     return (
         <Section title={'Rozwiązanie w arkuszu kalkulacyjnym'}>
-            <p className="ps-2">Bisekcja:</p>
+            <h3 className="ps-2 text-xl">Bisekcja:</h3>
             <iframe
                 className='w-full h-[600px] my-4 rounded-md'
                 frameborder="0"
                 src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQVZYPeIOH0Cd0OpG-eBt0494HKuvIbKHmMyQzo9dy3CN3fWoMOWOj6RiZt0BsxvbWMobhwgz6Ne8gS/pubhtml?widget=true&amp;headers=false">
             </iframe>
-            <p className="ps-2">Całka:</p>
+            <h3 className="ps-2 text-xl">Całka:</h3>
             <iframe
                 className='w-full h-[600px] my-4 rounded-md'
                 frameborder="0"
